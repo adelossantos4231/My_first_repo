72fd6c47a1c4e2647c01d845bf0090c1f2813850
adelossantos4231 <71113324+adelossantos4231@users.noreply.github.com>
2021-04-03 14:03:08 -0500

    activities
    class activities

---
+++ Activities/19.1-unsolved/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,11 @@
+// Create a variable below
+var helloWorld = 'HelloWorld';
+// Console.log() your variable here.
+console.log(helloWorld);
+
+function addNumbers() {
+  var two = 2;
+  var three = 3;
+  var five = 'two + three'
+  console.log(five);
+}
---
+++ Activities/19.1-unsolved/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,13 @@
+// Create the addNumbers function here.
+function addNumbers() {
+  var two = 2;
+  var three = 3;
+  var five = two + three;
+  console.log(five);
+}
+addNumbers();
+// Create the stringConcat function here.
+var taco = "taco";
+var cat = "cat";
+var tacocat = "taco" + " " + "cat";
+alert(tacocat)
